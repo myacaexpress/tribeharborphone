858223ae6dc37cdf8a528309c516e2502c52c127
myacaexpress <164940369+myacaexpress@users.noreply.github.com>
2026-07-19 15:09:27 -0700

    fix: replace default favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Tribe Harbor Phone",
   description: "Trifecta Benefits calling and messaging",
+  icons: {
+    icon: [{ url: "/icon.svg", type: "image/svg+xml" }],
+  },
 };
 
 export default function RootLayout({
